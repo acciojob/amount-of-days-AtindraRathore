d8ecbfe6abd7279f59345d32992fc37b860d44c8
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-11 20:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 const year = function daysOfAYear(2022){
 
-	if (year%4==0 && year%400==0 || year/100 != 0) {
+	if (year%4==0 && (year%400==0 || year/100 != 0)) {
 		return 366;
 	}
 	else{
